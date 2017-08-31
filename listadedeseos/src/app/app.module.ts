f59780dad29c61015e228c84b13381e1bbd58403
yenni <7095>
2017-08-31 17:41:25 -0300

    Se inicia el proyecto y se coloca los dos unicos componentes

--- listadedeseos/src/app/app.module.ts
+++ listadedeseos/src/app/app.module.ts
@@ -3,19 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { PendientesComponent } from '../pages/pendientes/pendientes.component';
-import { TerminadosComponent } from '../pages/terminados/terminados.component';
-
 @NgModule({
   declarations: [
     MyApp,
-    PendientesComponent,
-    TerminadosComponent,
+    AboutPage,
+    ContactPage,
+    HomePage,
     TabsPage
   ],
   imports: [
@@ -25,8 +26,9 @@ import { TerminadosComponent } from '../pages/terminados/terminados.component';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    PendientesComponent,
-    TerminadosComponent,
+    AboutPage,
+    ContactPage,
+    HomePage,
     TabsPage
   ],
   providers: [
